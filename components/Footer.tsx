d85c31085695639623863c09d7df2311826b562c
Ramanhajy <ramanhajy@gmail.com>
2026-02-06 17:22:21 +0300

    Initial commit - portfolio project

---
+++ components/Footer.tsx
@@ -0,0 +1,73 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { socialMedia } from "@/data";
+import MagicButton from "./MagicButton";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      {/* background grid */}
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how I can help you
+          achieve your goals.
+        </p>
+        <div className="flex flex-row gap-4">
+          <a href="mailto:ramanismaeel3@gmail.com">
+            <MagicButton
+              title="Let's get in touch"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+          <a
+            href="https://wa.me/9647731641021"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MagicButton
+              title="Whatsapp"
+              icon={
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path d="M20.52 3.48A12.07 12.07 0 0 0 12 0C5.37 0 0 5.37 0 12a11.93 11.93 0 0 0 1.64 6.06L0 24l6.31-1.66A12.09 12.09 0 0 0 12 24c6.63 0 12-5.37 12-12a11.93 11.93 0 0 0-3.48-8.52zM12 22a10.13 10.13 0 0 1-5.19-1.43l-.37-.22-3.75.99 1-3.65-.24-.38A10.08 10.08 0 1 1 12 22zm5.47-7.14c-.3-.15-1.77-.87-2.04-.97-.27-.1-.47-.15-.67.15-.2.3-.77.97-.94 1.17-.17.2-.35.22-.65.07-.3-.15-1.27-.47-2.42-1.5-.9-.8-1.51-1.78-1.69-2.08-.18-.3-.02-.46.13-.61.13-.13.3-.35.45-.52.15-.17.2-.3.3-.5.1-.2.05-.37-.02-.52-.07-.15-.67-1.62-.92-2.22-.24-.58-.5-.5-.67-.5-.17 0-.37-.02-.57-.02-.2 0-.52.07-.8.37-.27.3-1.05 1.02-1.05 2.5 0 1.47 1.08 2.9 1.23 3.1.15.2 2.13 3.25 5.17 4.42.72.28 1.28.45 1.72.58.72.23 1.38.2 1.9.12.58-.09 1.77-.72 2.02-1.42.25-.7.25-1.3.17-1.42-.08-.12-.27-.2-.57-.35z"/></svg>
+              }
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2025 Raman Ismael
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <a
+              key={info.id}
+              href={info.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,223 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I have had a strong passion for programming and design. I have experience developing web-based applications across different industries, including medicine, restaurants, tourism, and academic systems. My work includes building responsive user interfaces using modern frontend technologies.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Software Engineer Degree From University Technology Malaysia",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-3",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "I prioritize client collaboration, fostering open communication.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building personal projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cafe Management System",
+    des: "KurdCafe is a web-based cafe management system designed to manage orders and streamline daily cafe operations, with a focus on a clean interface and responsive design",
+    img: "/KurdCafe.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://kurdcafe.infinityfreeapp.com/",
+  },
+  {
+    id: 2,
+    title: "Dental Clinic System",
+    des: "DrShavinClinic is a web-based dental clinic management system that streamlines clinic operations by handling patient records, appointment scheduling, treatment tracking, and administrative workflows through a clean, responsive interface..",
+    img: "/dentalclinic.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://drshavinclinic.infinityfreeapp.com/",
+  },
+  {
+    id: 3,
+    title: "Online Ticket Booking System",
+    des: "Online Ticket Booking System is a web-based application that allows users to search for routes, select seats, and book tickets online through a responsive and user-friendly interface.",
+    img: "/ticket.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://onlineticketbooking.infinityfreeapp.com/",
+  },
+  {
+    id: 4,
+    title: "Pharmacy Management System",
+    des: "Pharmacy Management System is a web-based application designed to manage medicines, inventory, and daily pharmacy operations through a simple, efficient, and user-friendly interface.",
+    img: "/pharmacy.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Raman was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Raman's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Raman is the ideal partner.",
+    name: "Hoshyar Ali",
+    title: "Manager of KurdCafe",
+  },
+  {
+    quote:
+      "Raman was a pleasure to work with from start to finish. He’s reliable, responsive, and deeply invested in the success of the project. His genuine enthusiasm for development and problem-solving helped turn our ideas into a smooth, well-crafted website. An excellent choice for anyone looking to build or improve their brand online.",
+    name: "Shavin Abu Bakr",
+    title: "Owner of dental clinic",
+  },
+  {
+    quote:
+      "Working with Raman was a fantastic experience. He consistently demonstrated professionalism, strong attention to detail, and a clear commitment to high-quality results. His passion for development and ability to deliver on time made a real impact on our project. I highly recommend Raman to anyone looking to strengthen their digital presence.",
+    name: "Binar Mohammed Ali",
+    title: "Owner of BerlinShop",
+  },
+  {
+    quote:
+      "Raman brought both technical skill and creative thinking to our project. He approached every stage with care, professionalism, and a strong focus on delivering results. His dedication and positive attitude made collaboration easy and effective. I would confidently recommend him for any web development project.",
+    name: "Online Ticket Managing Team",
+    title: "Online Ticket Booking System",
+  },
+  {
+    quote:
+      "Raman is a highly dedicated and professional developer who delivers quality work with great attention to detail. His passion for development and commitment to excellence truly set him apart. I’d strongly recommend him to anyone looking to elevate their website and brand.",
+    name: "Koshyar Ako",
+    title: "Owner of the Pharmacy",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "JAVASCRIPT",
+    img: "/js.svg",
+    
+  },
+  {
+    id: 2,
+    name: "PHP",
+    img: "/php.svg",
+    
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "hostName.svg",
+  },
+  {
+    id: 4,
+    name: "LARAVEL",
+    img: "/laravel.svg",
+    
+  },
+  {
+    id: 5,
+    name: "HTML",
+    img: "/html-5.svg",
+    
+  },
+  {
+    id: 6,
+    name: "REACT",
+    img: "/react.svg",
+    
+  },
+  {
+    id: 7,
+    name: "CSS",
+    img: "/css.svg",
+    
+  },
+  {
+    id: 8,
+    name: "FIGMA",
+    img: "/figma.svg",
+    
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Engineer Industrial Training",
+    desc: "Assisted in the development of a web-based platform, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Developer",
+    desc: "Built web based systems across healthcare, restaurant, and service-based platforms using modern technologies",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Dev Project",
+    desc: "Led the dev of web applications for clients, from initial concept to deployment on hosting platforms.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Ramanhajy",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/raman-ismaeel",
+  },
+];
